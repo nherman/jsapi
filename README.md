4831e8a7c6c2dc399c3a6b1b46c7621b03ccce6b
Nick Herman <nherman@gmail.com>
2014-05-09 15:14:07 -0400

    * moved usage info to README  * renamed usage.html to example.html

--- README.md
+++ README.md
@@ -2,3 +2,50 @@ jsapi
 =====
 
 Generate Javascript methods that correspond to API endpoints
+
+Initializes API objects and stores them in a globally accessible map object
+called window.JSAPI.apis.  Each API consists of a collection of Endpoints.
+Each Endpoint contains four default methods: create(), read(), update(), delete().
+
+Usage:
+
+var newAPI = window.JSAPI.create(APIConfiguration);
+newAPI.endPointName.read({
+	urlData: {
+		"url1": "data"
+	},
+	paramData: {
+		"param1": "data"
+	},
+	callbacks: {
+		"onload": function() {},
+		"onerror": function() {}
+	}
+})
+
+APIConfiguration must be a JSON object that defines each endpoint in the API in this format:
+{
+	"options": {
+		"apiName": "APIContent",
+		"domain": "localhost",
+		"path": "api",
+		"methodTypes":[] //optional array of endpoint methods to use in addition to CRUD methods
+	},
+	"endpoints": {
+		"endpoint1": {
+			"pathTemplate":"",
+			"templateParams": [],
+			"update": {
+				"method": "POST",
+				"params": []
+			},
+			"read": {
+				"method": "GET"
+			},
+			"delete": {
+				"method": "DELETE",
+				"params": []
+			}
+		}
+	}
+}
--- jsapi.js
+++ jsapi.js
@@ -1,54 +1,7 @@
-/*
-JSAPI v0.1 (https://github.com/nherman/jsapi)
-MIT License: https://github.com/nherman/jsapi/blob/master/LICENSE
-
-Initializes API objects and stores them in a globally accessible map object
-called window.JSAPI.apis.  Each API consists of a collection of Endpoints.
-Each Endpoint contains four default methods: create(), read(), update(), delete().
-
-Usage:
-
-var newAPI = window.JSAPI.create(APIConfiguration);
-newAPI.endPointName.read({
-	urlData: {
-		"url1": "data"
-	},
-	paramData: {
-		"param1": "data"
-	},
-	callbacks: {
-		"onload": function() {},
-		"onerror": function() {}
-	}
-})
-
-APIConfiguration must be a JSON object that defines each endpoint in the API in this format:
-{
-	"options": {
-		"apiName": "APIContent",
-		"domain": "localhost",
-		"path": "api",
-		"methodTypes":[] //optional array of endpoint methods to use in addition to CRUD methods
-	},
-	"endpoints": {
-		"endpoint1": {
-			"pathTemplate":"",
-			"templateParams": [],
-			"update": {
-				"method": "POST",
-				"params": []
-			},
-			"read": {
-				"method": "GET"
-			},
-			"delete": {
-				"method": "DELETE",
-				"params": []
-			}
-		}
-	}
-}
-*/
+/***
+ * JSAPI v0.1 (https://github.com/nherman/jsapi)
+ * MIT License: https://github.com/nherman/jsapi/blob/master/LICENSE
+ ***/
 
 window.JSAPI = window.JSAPI || (function() {
 	"use strict";
