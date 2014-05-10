fac9046341a89f714f16958d5d372aa75af2684f
Nick Herman <nherman@gmail.com>
2014-05-09 22:39:37 -0400

    * fleshing out examples

--- config_template.js
+++ config_template.js
@@ -1,65 +1,67 @@
-/* API Config Template */
+/* Example API Config */
 
-window.API_CONFIG = window.API_CONFIG || {};
-window.API_CONFIG.api1 = {
-	"options": {
-		"apiName": "API1",
-		"domain": "localhost",
-		"path": "api1"				//prepend this value to the endpoint path
-	},
-	"endpoints": {
-		"endpoint1": {
-			"pathTemplate":"/crud/%s",
-			"templateParams": ["urlparam"],
-			"requestHeaders": {
-				"Content-type": "application/rtf",	//override the content-type by default
-				"Authentication": ""				//allow caller to write Authentication header
-			},
-			"update": {
-				"method": "POST",
-				"params": ["updateParam1", "updateParam2"]
-			},
-			"read": {
-				"method": "GET",
-				"params": ["readParam1", "readParam2"]
-			}
+(function() {
+	window.MYAPIS = window.MYAPIS || {};
+	window.MYAPIS.api1 = JSAPI.create({
+		"options": {
+			"apiName": "API1",
+			"domain": "localhost",
+			"path": "api1"				//prepend this value to the endpoint path
 		},
-		"endpoint2": {
-			"pathTemplate":"/crud/ep2",
-			"create": {
-				"method": "POST",
-				"params": ["createParam1", "createParam2"]
+		"endpoints": {
+			"endpoint1": {
+				"pathTemplate":"/crud/%s",
+				"templateParams": ["urlparam"],
+				"requestHeaders": {
+					"Content-type": "application/rtf",	//override the content-type by default
+					"Authentication": ""				//allow caller to write Authentication header
+				},
+				"update": {
+					"method": "POST",
+					"params": ["updateParam1", "updateParam2"]
+				},
+				"read": {
+					"method": "GET",
+					"params": ["readParam1", "readParam2"]
+				}
 			},
-			"read": {
-				"method": "GET",
-				"params": ["readParam1", "readParam2"]
-			},
-			"delete": {
-				"method": "DELETE",
-				"params": []
+			"endpoint2": {
+				"pathTemplate":"/crud/ep2",
+				"create": {
+					"method": "POST",
+					"params": ["createParam1", "createParam2"]
+				},
+				"read": {
+					"method": "GET",
+					"params": ["readParam1", "readParam2"]
+				},
+				"delete": {
+					"method": "DELETE",
+					"params": []
+				}
 			}
 		}
-	}
-};
+	});
 
-window.API_CONFIG.api2 = {
-	"options": {
-		"apiName": "API2",
-		"protocol": "https",
-		"domain": "domain-for-api2.com",
-		"path": "api2",
-		"methodTypes":["stream"]	//optional array of endpoint methods to use in addition to CRUD methods
-	},
-	"endpoints": {
-		"stream": {
-			"pathTemplate":"/stream",
+	window.MYAPIS.api2 = JSAPI.create({
+		"options": {
+			"apiName": "API2",
+			"protocol": "https",
+			"domain": "domain-for-api2.com",
+			"path": "api2",
+			"methodTypes":["stream"]	//optional array of endpoint methods to use in addition to CRUD methods
+		},
+		"endpoints": {
 			"stream": {
-				"myoption": 1
+				"pathTemplate":"/stream",
+				"stream": {
+					"myoption": 1
+				}
 			}
 		}
-	}
-};
+	});
 
+}());
 
 
 /*
